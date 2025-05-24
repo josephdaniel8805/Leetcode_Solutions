0704fa1e3bb8c0f8a55cd7a7d0fc84ad2bb63db7
Joseph Daniel <josephdaniel8805@gmail.com>
2025-05-24 22:38:14 +0530

    Solution of 2942 - Find Words Containing Character - Optimal Approach - Java

---
+++ 2942-Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    //Optimal Approach
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(words[i].indexOf(x)!=-1){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
