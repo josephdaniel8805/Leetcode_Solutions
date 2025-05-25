8a62b55ebf888d38f97429295f8c45dd10c46807
Joseph Daniel <josephdaniel8805@gmail.com>
2025-05-25 22:58:28 +0530

    Solution of 704 - Binary Search - Recursive Approach - Java

---
+++ 704-Solution_2.java
@@ -0,0 +1,19 @@
+class Solution {
+    //Recursive Approach
+    public int search(int[] nums, int target){
+        int n=nums.length;
+        int low=0,high=n-1;
+        return bin(low,high,target,nums);
+    }
+    public int bin(int low,int high,int target,int[] nums){
+        if(low>high) return -1;
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]<target){
+            return bin(mid+1,high,target,nums);
+        }else{
+            return bin(low,mid-1,target,nums);
+        }
+    }
+}
