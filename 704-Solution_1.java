dcf81a605fb996959ca3ca8010038ba03559e613
Joseph Daniel <josephdaniel8805@gmail.com>
2025-05-25 23:04:08 +0530

    Solution of 704 - Binary Search - Non-Recursive Approach - Java

--- 704-Solution_1.java
+++ 704-Solution_1.java
@@ -1,5 +1,5 @@
 class Solution {
-    //Non-Recursive Approach
+    //Non-Recursive Approach 
     public int search(int[] nums, int target) {
         int n=nums.length;
         int low=0,high=n-1;
