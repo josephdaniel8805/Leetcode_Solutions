e26a1c9329ad01735d10012378e0e90c6552c069
Joseph Daniel <josephdaniel8805@gmail.com>
2025-06-01 18:36:22 +0530

    Solution of 1108 - Defanging an IP Address - Java

---
+++ 1108-Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder ans = new StringBuilder();
+        for(char c : address.toCharArray()){
+            if(c=='.'){
+                ans.append("[.]");
+            }else{
+                ans.append(c);
+            }
+        }
+        return ans.toString();
+        //return address.replaceAll("\\.","[.]");
+    }
+}
