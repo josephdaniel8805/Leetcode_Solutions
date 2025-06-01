cd5fc4ea4ff2c2aca8d1f920614a6174ac6cb82f
Joseph Daniel <josephdaniel8805@gmail.com>
2025-06-01 20:07:36 +0530

    Solution of 1431 - Kids With the GReatest Number of Candies - Java

---
+++ 1431-Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> result= new ArrayList<>();
+        int max=0;
+        for(int i : candies){
+            max=(max<i)?i:max;
+        }
+        for(int i : candies){
+            if((i+extraCandies)>=max){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
