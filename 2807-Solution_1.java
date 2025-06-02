438d946ac2009b9dd7729e3af0143cea239cee54
Joseph Daniel <josephdaniel8805@gmail.com>
2025-06-02 21:51:02 +0530

    Solution of 2807 - Insert Greatest Common Divisors in Linked List - BigInteger Approach - Java

---
+++ 2807-Solution_1.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.*;
+import java.math.BigInteger;
+class Solution {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    //BigInteger Approach
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode ptr=head;
+        ListNode ptr1=ptr.next;
+        while(ptr!=null && ptr1!=null){
+            int gcd = BigInteger.valueOf(ptr.val).gcd(BigInteger.valueOf(ptr1.val)).intValue();
+            ListNode gcdnode=new ListNode(gcd);
+            gcdnode.next=ptr1;
+            ptr.next=gcdnode;
+            ptr=ptr1;
+            ptr1=ptr1.next;
+        }
+        return head;
+    }
+}
