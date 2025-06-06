6129efe501de3e2edfd238d9eeb4098354bcda9e
Joseph Daniel <josephdaniel8805@gmail.com>
2025-06-06 15:02:24 +0530

    Solution of 1769 - Minimum Number of Operations to Move All Balls to Each Box- Optimal Approach - Java

---
+++ 1769-Solution_2.java
@@ -0,0 +1,23 @@
+class Solution {
+    //Optimal Approach
+    public int[] minOperations(String boxes) {
+        int n=boxes.length();
+        int ans[]=new int[n];
+        int sum=0;
+        int operations=0,count=0;
+        for(int i=0;i<n;i++){
+            ans[i]+=operations;
+            count+=boxes.charAt(i) -'0';
+            operations+=count;
+        }
+        operations=0;
+        count=0;
+        for(int i=n-1;i>=0;i--){
+            ans[i]+=operations;
+            count+=boxes.charAt(i)-'0';
+            operations+=count;
+        }
+        return ans;
+        
+    }
+}
