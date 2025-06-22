3605f992e257a494df9b73392ddc1bf11bc5241c
Joseph Daniel <josephdaniel8805@gmail.com>
2025-06-22 18:38:36 +0530

    Solution of 2138 - Divide a String Into Groups of Size k - Java

---
+++ 2138-Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        List<String> words = new ArrayList<>();
+        StringBuilder sb=new StringBuilder();
+        int j=0,ptr=0;
+        for(int i=0;i<s.length();i++){
+            sb.append(s.charAt(i));
+            ptr++;
+            if(ptr==k){
+                words.add(sb.toString());
+                sb.setLength(0);
+                ptr=0;
+            }
+        }
+        while(ptr!=k && ptr!=0){
+            sb.append(fill);
+            ptr++;
+            if(ptr==k){
+                words.add(sb.toString());
+            }
+        }
+        return words.toArray(new String[0]);
+    }
+}
