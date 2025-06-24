0a68ae4f1b463178778f5c54c7ae3d54d961c236
Joseph Daniel <josephdaniel8805@gmail.com>
2025-06-24 21:56:10 +0530

    Solution of 2200 - Find All K-Distant Indicies in an Array - Optimal Force Approach - Java

---
+++ 2200-Solution_2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    //Optimal Approach
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        int n=nums.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        boolean[] seen=new boolean[n];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==key){
+                int start=Math.max(0,i-k);
+                int end=Math.min(n-1,i+k);
+                for(int j=start;j<=end;j++){
+                    seen[j]=true;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(seen[i]){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
