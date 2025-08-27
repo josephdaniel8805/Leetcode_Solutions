c6bae891ced9b2d516b62224b8d7719bf6f61324
Joseph Daniel <josephdaniel8805@gmail.com>
2025-08-27 23:10:36 +0530

    Solution of 97 - Interleaving String - Optimal Solution - Java

---
+++ 97-Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public boolean isInterleave(String s1,String s2,String s3){
+        int i1=0,i2=0,i3=0;
+        Boolean dp[][]=new Boolean[s1.length()][s2.length()];
+        if(s1.length()+s2.length()!=s3.length()){
+            return false;
+        }
+        return isValid(i1, i2, i3, s1, s2, s3,dp);
+    }
+    private boolean isValid(int i1,int i2,int i3,String s1,String s2,String s3, Boolean dp[][]){
+        if(i3==s3.length()) return true;
+        if(i1<s1.length() && i2<s2.length() && dp[i1][i2]!=null) return dp[i1][i2];
+        if(i1<s1.length() && i2<s2.length() && s1.charAt(i1)==s3.charAt(i3) && s2.charAt(i2)==s3.charAt(i3)){
+            boolean result= isValid(i1+1, i2, i3+1, s1, s2, s3,dp);
+            if(result){
+                dp[i1][i2]=true;
+                return true;
+            }
+            return isValid(i1, i2+1, i3+1, s1, s2, s3,dp);
+        }else if(i2<s2.length() && s2.charAt(i2)==s3.charAt(i3)){
+            boolean result = isValid(i1, i2+1, i3+1, s1, s2, s3,dp);
+            if(i1<s1.length()){
+                dp[i1][i2]=result;
+            }
+            return result;            
+        }else if(i1<s1.length() && s1.charAt(i1)==s3.charAt(i3)){
+            boolean result= isValid(i1+1, i2, i3+1, s1, s2, s3,dp);
+            if(i2<s2.length()){
+                dp[i1][i2]=result;
+            }
+            return result;
+        }else{
+            if(i1<s1.length() && i2<s2.length()){
+                dp[i1][i2]=false;
+            }
+        }
+        return false;
+    }
+}
