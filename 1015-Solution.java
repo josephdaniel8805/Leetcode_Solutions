984f3e6c54681263c273a17055e6a4e9111b3ce5
Joseph Daniel <josephdaniel8805@gmail.com>
2025-11-25 20:59:30 +0530

    Solution of 1015 - Smallest Integer Divisible by K - Java

---
+++ 1015-Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if(k%2==0 || k%5==0) {
+            return -1;
+        }
+        int num=1,ans=1;
+        while(num%k!=0){
+            num=(num*10+1)%k;
+            ans++;   
+        }
+        return ans;
+    }
+}
