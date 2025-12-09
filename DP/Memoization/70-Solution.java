aeae3017910a7b7d41bfb9dd29f0d6de6207d917
Joseph Daniel <josephdaniel8805@gmail.com>
2025-12-09 10:22:15 +0530

    Solution of 70 - Climbing Stairs - Java

---
+++ DP/Memoization/70-Solution.java
@@ -0,0 +1,11 @@
+package DP.Memoization;
+
+class Solution {
+    int arr[] = new int[46];
+    public int climbStairs(int n) {
+        if(n<=1) return 1;        
+        if(arr[n]!=0) return arr[n];  
+        arr[n]=climbStairs(n-1)+climbStairs(n-2);
+        return arr[n];
+    }
+}
