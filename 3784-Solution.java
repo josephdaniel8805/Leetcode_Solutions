ee455f49b467504d25f57f0f60529e77a5eae2d1
Joseph Daniel <josephdaniel8805@gmail.com>
2025-12-21 23:07:15 +0530

    Solution of 3784 - Minimum Deletion Cost to Make All Characters Equal - Java

---
+++ 3784-Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public long minCost(String s, int[] cost){
+        HashMap<Character,Long> map = new HashMap<>();
+        int n=cost.length;long max=0,total=0;
+        for(int ind=0;ind<n;ind++){
+            char ch = s.charAt(ind);
+            long temp=(long)cost[ind];
+            total+=temp;
+            map.put(ch,map.getOrDefault(ch,0L)+temp);
+            if(max<map.getOrDefault(ch,0L)){
+                max=map.getOrDefault(ch,0L);
+            }
+        }
+        long ans=0;
+        for(Map.Entry<Character,Long> entry : map.entrySet()){
+            max=Math.max(max,entry.getValue());
+        }
+        return total-max;
+    }
+}
