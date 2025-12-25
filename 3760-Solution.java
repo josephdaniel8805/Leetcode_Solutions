b3999da6ee85a78c5e3a20eea93a13498d221af9
Joseph Daniel <josephdaniel8805@gmail.com>
2025-12-25 23:23:56 +0530

    Solution of 3760 - Maximum Substrings With Distinct Start - Java

---
+++ 3760-Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int maxDistinct(String s) {
+        HashSet<Character> set = new HashSet<>();
+        for(char ch : s.toCharArray()){
+            set.add(ch);
+        }
+        return set.size();
+    }
+}
