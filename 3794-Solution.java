f0a077ce759a762040569a5d5e53dabd834458d1
Joseph Daniel <josephdaniel8805@gmail.com>
2026-01-03 23:03:52 +0530

    Solution of 3794 - Rverse String Prefix - Java

---
+++ 3794-Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String reversePrefix(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for(int ind=k-1;ind>=0;ind--){
+            sb.append(s.charAt(ind));
+        }
+        return sb.append(s.substring(k,s.length())).toString();
+    }
+}
