cf75a04cf1ec1c38c277feae252d9361e5ef77bb
Joseph Daniel <josephdaniel8805@gmail.com>
2026-01-20 23:21:44 +0530

    Solution of 3314 - Construxt the Minimum Bitwise Array I - Java

---
+++ 3314-Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n=nums.size();
+        int ans[]= new int[n];
+        for(int ind=0;ind<n;ind++){
+            int or=-1,num=nums.get(ind);
+            for(int j=1;j<num;j++){
+                if((j|(j+1))==num){
+                    or=j;
+                    break;
+                }
+            }
+            ans[ind]=or;
+        }
+        return ans;
+    }
+}
