3462160599e04623c14e1e6e267ac46671992557
Joseph Daniel <josephdaniel8805@gmail.com>
2026-01-25 23:44:19 +0530

    Solution of 1984 - Minimum Difference Between Highest and Lowest of K Scores - Java

---
+++ 1984-Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if(k<=1) return 0;
+        Arrays.sort(nums);
+        int ans=Integer.MAX_VALUE;k--;
+        for(int ind=nums.length-1;(ind-k)>=0;ind--){
+            if(ans>(nums[ind]-nums[ind-k])){
+                ans=nums[ind]-nums[ind-k];
+            }
+        }
+        return ans;
+    }
+}
